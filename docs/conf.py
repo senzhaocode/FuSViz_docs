83c4ea8e112d925442b5e6497bb46daa5581c9a9
Sen ZHAO <t.cytotoxic@gmail.com>
2024-02-11 19:11:41 +0100

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = u'2022, Sen ZHAO'
 author = u'Sen ZHAO'
 
 # The short X.Y version
-version = u'1.0'
+version = u'1.4'
 # The full version, including alpha/beta/rc tags
-release = u'1.0'
+release = u'1.4'
 
 
 # -- General configuration ---------------------------------------------------
